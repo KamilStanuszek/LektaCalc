bb7fd701b7a9f238f77e75e2852cd5d60f7ed102
Kamil Stanuszek <kamil.stanuszek@kissdigital.com>
2020-02-08 09:56:41 +0100

    Initial Commit

---
+++ LektaCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LektaCalc
+//
+//  Created by Kamil Stanuszek on 08/02/2020.
+//  Copyright © 2020 Kamil Stanuszek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
